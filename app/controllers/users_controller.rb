576622a72bb95fa93640f275ba4312997ce56e5f
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-08-30 11:41:32 -0600

    Implement users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User has been created'
+      redirect_to user_path(@user)
+    else
+      flash[:danger] = 'User has not been created'
+      render :new
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+end
