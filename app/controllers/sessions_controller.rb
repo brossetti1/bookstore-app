9e3026eb2e60eb889af261eccbc7558ff8bcf7e6
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-09-07 12:21:37 -0600

    Implement signing users out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
       flash[:success] = "Sign in successful"
@@ -13,5 +12,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = "Invalid email/password combination"
       render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have been signed out"
+    redirect_to root_path
   end
 end
