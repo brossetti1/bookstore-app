8769d3a6535bb85586fbb7970e908f7f8a62d10c
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-09-07 08:52:11 -0600

    Create new and create session controller specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "catalogs#index"
 
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/signin', to: 'sessions#create'
+
   resources :authors
   resources :books
   resources :users, only: [:index, :show, :new, :create]
+
 end
