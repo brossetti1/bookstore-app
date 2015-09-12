1e39994939d1eaf804467ec32540b6e91589b5f3
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-09-12 06:36:21 -0600

    Implement Publishers Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
   get '/signin', to: 'sessions#new', as: 'signin'
   post '/signin', to: 'sessions#create'
+  get '/add_publisher', to: 'publishers#new', as: 'add_publisher'
 
   resources :authors
   resources :books
   resources :users, only: [:index, :show, :new, :create]
   resource :session
+  resources :publishers, except: [:new]
 
 end
